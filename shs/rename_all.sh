cb6a820afafe789082641d50f5b74249a5fdb782
metasmile <cyrano905@gmail.com>
2015-11-09 17:17:58 +0900

    Squashed 'scripts/' changes from cc1ba4d..6ef6b3c
    6ef6b3c Merge commit '53827fc7aa000ec5ea43400fd90390122380e792'
    53827fc Squashed 'ai2icons/' changes from 2557bac..2cdf21c
    127854b optimize scheme of icon file name
    145ecc5 Update rename_all.sh
    70b939e overwrites the file contents only matched string
    
    git-subtree-dir: scripts
    git-subtree-split: 6ef6b3c48da31b1e51aa296a85cc17e0d3a60550

--- shs/rename_all.sh
+++ shs/rename_all.sh
@@ -3,4 +3,4 @@ mvcmd+="$1"
 mvcmd+="/"
 mvcmd+="$2"
 mvcmd+='}"; done'
-find . -depth -name "*$1*" -not -path "./.git/*" -exec bash -c mvcmd _ {} +
+find . -depth -name "*$1*" -not -path "./.git/*" -exec bash -c "$mvcmd" _ {} +
